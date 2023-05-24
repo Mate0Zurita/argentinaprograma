db11bcc3d779d6ea345b5707bbd95008ec433555
Mateo Zurita <mateo.zurita@mi.unc.edu.ar>
2023-05-23 21:43:49 -0300

    Practico N0 finalizado

---
+++ practico0/promedio3.js
@@ -0,0 +1,17 @@
+let n1 = 5;
+let n2 = 7;
+let n3 = 9;
+let promedio = (n1+n2+n3)/3;
+console.log(promedio);
+
+n1 = -2;
+n2 = -3;
+n3 = -8;
+promedio = (n1+n2+n3)/3;
+console.log(promedio);
+
+n1 = -1.2;
+n2 = 5.3;
+n3 = -3.3;
+promedio = (n1+n2+n3)/3;
+console.log(promedio);
