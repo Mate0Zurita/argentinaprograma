a0421abc19df09a4ab7033ec9811e1905f2b76f2
Mate0Zurita <134146374+Mate0Zurita@users.noreply.github.com>
2023-06-13 15:21:35 -0300

    Agrego archivo miNombreCompleto.js

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,9 @@
+let nombre;
+nombre = "Mateo";
+let apellido = "Zurita";
+let edad = 22;
+
+console.log("Hola %s %s. Tienes %d años.",nombre,apellido,edad);
+//Nota mental: %s es para string, %d para numeros, luego hace falta poner los valores en comas
+
+// Imprime en consola: "Hola Mateo Zurita. Tienes 22 años."
