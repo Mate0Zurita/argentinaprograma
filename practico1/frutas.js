af9d10b1f770abbc5e39109e44c44e39089f9f8e
Mate0Zurita <134146374+Mate0Zurita@users.noreply.github.com>
2023-06-13 16:38:52 -0300

    Agregué "frutas.js"

---
+++ practico1/frutas.js
@@ -0,0 +1,18 @@
+// a)
+let frutas = [];
+
+// b)
+f1="banana";
+f2="pera";
+f3="sandia";
+f4="naranja"
+f5="uva";
+frutas.push(f1,f2,f3,f4,f5);
+
+// c)
+let ultima = frutas[frutas.length-1];
+console.log("La ultima fruta agregada fue: %s",ultima);
+
+// d)
+let primera = frutas[0]
+console.log("La primera fruta agregada fue: %s", primera)
