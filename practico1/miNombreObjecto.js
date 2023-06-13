a0f7de60ed0c53f5e23b51d48102ca7b47559c25
Mate0Zurita <134146374+Mate0Zurita@users.noreply.github.com>
2023-06-13 16:37:21 -0300

    Agregué "miNombreObjecto.js"

---
+++ practico1/miNombreObjecto.js
@@ -0,0 +1,6 @@
+let persona = {nombre: "Mateo", apellido: "Zurita", edad: "22"};
+
+console.log("Mi objeto persona es el siguiente:", JSON.stringify(persona));
+// el JSON.stringify convierte a string todo el objeto persona.
+
+// obtengo por consola: Mi objeto persona es el siguiente: {"nombre":"Mateo","apellido":"Zurita","edad":"22"}
